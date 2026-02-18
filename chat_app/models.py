53bf412a2da38983311457a70e4d4fce51ccfd8d
yunikpandey <yunikpd@gmail.com>
2026-02-18 13:23:58 +0545

    models class are sender,content,timestamp

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    sender = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp= models.DateTimeField(auto_now_add=True)
