52fa230486922798be3c2619e46ea22520a5d61f
yunikpandey <yunikpd@gmail.com>
2026-02-18 13:59:46 +0545

    added Templates name as templates

--- CHAT/settings.py
+++ CHAT/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CHAT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
