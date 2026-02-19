2c98b4fe3740c5e4e05cebca550973f3f34cb5d7
yunikpandey <yunikpd@gmail.com>
2026-02-19 12:19:40 +0545

    Added login and logout link

--- CHAT/settings.py
+++ CHAT/settings.py
@@ -115,4 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
